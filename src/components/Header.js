f14e2a289e63040a2767b629b5ceba8c5b538127
PierreM <7anUDQ2A3MVJkC7J@gmail.com>
2021-12-04 22:56:22 +0100

    header fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
 import React from "react";
+import banner from "../images/banner.png";
+import "./Header.css";
 
 // import allocine from "https://assets.allocine.fr/skin/img/allocine/logo-main-5f12bf0a15.svg";
 
@@ -6,10 +8,7 @@ const Header = () => {
   return (
     <>
       <header>
-        <img
-          src="https://assets.allocine.fr/skin/img/allocine/logo-main-5f12bf0a15.svg"
-          alt="allocine"
-        />
+        <img src={banner} alt="allocine" className="banner" />
       </header>
     </>
   );
